27ede7b1f1321e18e7d1017e65b6a98368ed115d
Francisco García <fco.garcia.delgado@gmail.com>
2022-07-31 10:29:57 +0200

    login service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import {RouterModule, Routes } from '@angular/router';
 
 import { AcercaDeNosotrosComponent } from './views/acerca-de-nosotros/acerca-de-nosotros.component';
 import { ListadoComponent } from './views/listado/listado.component';
+import { LoginComponent } from './views/login/login.component';
 import { PaginaNoEncontradaComponent } from './views/pagina-no-encontrada/pagina-no-encontrada.component';
 
 const  routes:  Routes  = [
+  { path: 'login', component: LoginComponent },
   { path: 'listado', component: ListadoComponent },
   { path: 'nosotros', component: AcercaDeNosotrosComponent},
   { path: '', redirectTo: '/listado', pathMatch: 'full'},
